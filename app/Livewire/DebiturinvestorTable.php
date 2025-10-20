f2098d323e08718b0761c7fa29e42ebdbe882ed2
Sulaiman Saleh <manz.iman99@gmail.com>
2025-10-20 11:29:26 +0700

    fix

--- app/Livewire/DebiturinvestorTable.php
+++ app/Livewire/DebiturinvestorTable.php
@@ -6,7 +6,7 @@ use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\MasterDebiturDanInvestor;
 
-class DebiturInvestorTable extends DataTableComponent
+class DebiturInvestorTables extends DataTableComponent
 {
     protected $model = MasterDebiturDanInvestor::class;
 
@@ -20,15 +20,15 @@ class DebiturInvestorTable extends DataTableComponent
             ->setSearchEnabled()
             ->setSearchPlaceholder('Cari Debitur/Investor...')
             ->setSearchDebounce(500)
-            
+
             // Pagination
             ->setPerPageAccepted([10, 25, 50, 100])
             ->setPerPageVisibilityEnabled()
             ->setPerPage(10)
-            
+
             // Default Sort
             ->setDefaultSort('id_debitur', 'asc')
-            
+
             // Table Styling
             ->setTableAttributes([
                 'class' => 'table table-hover',
@@ -43,7 +43,7 @@ class DebiturInvestorTable extends DataTableComponent
             ->setPerPageFieldAttributes([
                 'class' => 'form-select',
             ])
-            
+
             // Disable Bulk Actions
             ->setBulkActionsDisabled();
     }
@@ -61,70 +61,70 @@ class DebiturInvestorTable extends DataTableComponent
         $rowNumber = 0;
         return [
             Column::make("No")
-                ->label(function($row) use (&$rowNumber) {
+                ->label(function ($row) use (&$rowNumber) {
                     $rowNumber++;
                     $number = (($this->getPage() - 1) * $this->getPerPage()) + $rowNumber;
                     return '<div class="text-center">' . $number . '</div>';
                 })
                 ->html()
                 ->excludeFromColumnSelect(),
-            
+
             Column::make("Nama Debitur/Investor", "nama_debitur")
                 ->sortable()
                 ->searchable()
                 ->format(fn($value) => '<div class="text-center">' . ($value ?? '-') . '</div>')
                 ->html(),
-            
+
             Column::make("Flagging", "flagging")
                 ->sortable()
                 ->searchable()
-                ->format(function($value) {
-                    $badge = ($value === 'ya') 
-                        ? '<span class="badge bg-label-success">Investor</span>' 
+                ->format(function ($value) {
+                    $badge = ($value === 'ya')
+                        ? '<span class="badge bg-label-success">Investor</span>'
                         : '<span class="badge bg-label-secondary">-</span>';
                     return '<div class="text-center">' . $badge . '</div>';
                 })
                 ->html(),
-            
+
             Column::make("Nama CEO", "nama_ceo")
                 ->sortable()
                 ->searchable()
                 ->format(fn($value) => '<div class="text-center">' . ($value ?? '-') . '</div>')
                 ->html(),
-            
+
             Column::make("Alamat", "alamat")
                 ->sortable()
                 ->searchable()
                 ->format(fn($value) => '<div class="text-center">' . ($value ?? '-') . '</div>')
                 ->html(),
-            
+
             Column::make("Email", "email")
                 ->sortable()
                 ->searchable()
                 ->format(fn($value) => '<div class="text-center">' . ($value ?? '-') . '</div>')
                 ->html(),
-            
+
             Column::make("KOL Perusahaan", "id_kol")
                 ->sortable()
                 ->searchable()
-                ->format(function($value, $row) {
+                ->format(function ($value, $row) {
                     $kolValue = optional($row->kol)->kol ?? '-';
                     return '<div class="text-center">' . $kolValue . '</div>';
                 })
                 ->html(),
-            
+
             Column::make("Nama Bank", "nama_bank")
                 ->sortable()
                 ->searchable()
                 ->format(fn($value) => '<div class="text-center">' . ($value ?? '-') . '</div>')
                 ->html(),
-            
+
             Column::make("No Rekening", "no_rek")
                 ->sortable()
                 ->searchable()
                 ->format(fn($value) => '<div class="text-center">' . ($value ?? '-') . '</div>')
                 ->html(),
-            
+
             Column::make("Aksi")
                 ->label(fn($row) => view('livewire.master-data-debitur-investor.partials.table-actions', ['id' => $row->id_debitur]))
                 ->html()
