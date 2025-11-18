a4a173fb290464b77602073b8c523c773e1fed10
Kimi no namae wa <imamyabunaiya13@gmail.com>
2025-11-18 21:20:33 +0700

    fix : route livewire

--- routes/livewire_route.php
+++ routes/livewire_route.php
@@ -8,7 +8,7 @@ Route::get('dashboard', Dashboard::class)->name('dashboard.index');
 
 Route::get('/master-data/kol', \App\Livewire\MasterData\MasterKol::class)->name('master-data.kol.index');
 Route::get('/master-data/sumber-pendanaan-eksternal', \App\Livewire\MasterData\SumberPendanaanEksternal::class)->name('master-data.sumber-pendanaan-eksternal.index');
-Route::get('/master-data/debitur-dan-investor', \App\Livewire\MasterData\DebiturDanInvestor::class)->name('master-data.debitur-dan-investor.index');
+Route::get('/master-data/debitur-investor', \App\Livewire\MasterData\DebiturDanInvestor::class)->name('master-data.debitur-investor.index');
 Route::get('/master-data/master-karyawan-ski', \App\Livewire\MasterData\MasterKaryawanSki::class)->name('master-data.master-karyawan-ski.index');
 
 Route::get('config-matrix-pinjaman', \App\Livewire\ConfigMatrixPinjaman\ConfigMatrixPinjamanIndex::class)->name('config-matrix-pinjaman.index');
