32126dec6a377a8bbe6025f73de33222741fa1dc
mfaqihh <mohammadfaqih@student.telkomuniversity.ac.id>
2025-11-28 13:39:14 +0700

    feat: halaman pengembalian investasi

---
+++ app/Livewire/PengembalianInvestasi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PengembalianInvestasi extends Component
+{
+    public function render()
+    {
+        return view('livewire.pengembalian-investasi.index');
+    }
+}
