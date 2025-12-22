ec61134d8a4b765c3105abdb8dc581d5526f78f1
mfaqihh <mohammadfaqih@student.telkomuniversity.ac.id>
2025-12-22 23:34:51 +0700

    fix: Feedback modul pengembalian

--- app/Livewire/DebiturPiutangIndex.php
+++ app/Livewire/DebiturPiutangIndex.php
@@ -2,10 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Exports\DebiturPiutangExport;
 use Livewire\Component;
+use Maatwebsite\Excel\Facades\Excel;
 
 class DebiturPiutangIndex extends Component
 {
+    public $searchTerm = '';
+
+    protected $listeners = ['search-changed' => 'updateSearch'];
+
+    public function updateSearch($value)
+    {
+        $this->searchTerm = $value;
+    }
+
+    public function export()
+    {
+        $filename = 'Debitur_Piutang_' . date('Y-m-d_His') . '.xlsx';
+
+        return Excel::download(
+            new DebiturPiutangExport($this->searchTerm),
+            $filename
+        );
+    }
+
     public function render()
     {
         return view('livewire.debitur-piutang.index')
--- resources/views/livewire/report-pengembalian/index.blade.php
+++ resources/views/livewire/report-pengembalian/index.blade.php
@@ -5,6 +5,9 @@
         <div class="col-12">
             <div class="mb-4 d-flex justify-content-between align-items-center">
                 <h4 class="fw-bold">Report Pengembalian</h4>
+                <a href="{{ route('report-pengembalian.export-pdf') }}" class="btn btn-success" target="_blank">
+                    <i class="fa-solid fa-file-pdf me-1"></i> Export PDF
+                </a>
             </div>
         </div>
     </div>
@@ -13,11 +16,6 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <div class="d-flex justify-content-end mb-3">
-                        <a href="{{ route('report-pengembalian.export-pdf') }}" class="btn btn-outline-secondary" target="_blank">
-                            <i class="fa-solid fa-file-pdf me-1"></i> Export PDF
-                        </a>
-                    </div>
                     <livewire:report-pengembalian-table />
                 </div>
             </div>
