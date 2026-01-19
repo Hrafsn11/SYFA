068fbc7d9ee7794c7644d0d8b062158cf006634c
i_miss_her <imamyabunaiya13@gmail.com>
2026-01-19 13:49:27 +0700

    fix : pengembalian peminjaman

--- resources/views/livewire/pengembalian-pinjaman/partials/table-actions.blade.php
+++ resources/views/livewire/pengembalian-pinjaman/partials/table-actions.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex justify-content-center align-items-center gap-2">
-    <a href="{{ route('pengembalian.detail', $id) }}"
+    <a href="{{ $route_detail }}"
         class="btn btn-sm btn-outline-primary" title="Detail Pengembalian Pinjaman">
         <i class="ti ti-file"></i>
 
