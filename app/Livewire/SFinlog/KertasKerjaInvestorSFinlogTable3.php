ce024bf34a3b39dea2f08f9cd79e3e700b35b752
HRA <hadi11maestro@gmail.com>
2026-01-26 11:55:57 +0700

    fix : penamaan

--- app/Livewire/SFinlog/KertasKerjaInvestorSFinlogTable3.php
+++ app/Livewire/SFinlog/KertasKerjaInvestorSFinlogTable3.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
 use App\Models\PengajuanInvestasiFinlog;
 
-class KertasKerjaInvestorSFinlogTable3 extends DataTableComponent
+class KertasKerjaInvestorSfinlogTable3 extends DataTableComponent
 {
     protected $model = PengajuanInvestasiFinlog::class;
 
